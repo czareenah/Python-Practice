d9829fa5757513b2a8519faaeae8a68b331ac3c9
czareenah <czareenahmagday@gmail.com>
2024-02-11 02:37:30 +0000

    notes v2

--- gui-practice-grid system.py
+++ gui-practice-grid system.py
@@ -5,14 +5,16 @@ from tkinter import *
 root = Tk()
 
 # Define - Creating a label widget 
-myLabel1 = Label(root, text="Hello World!")
-myLabel2 = Label(root, text="Goodbye World!")
-myLabel3 = Label(root, text="I'll be back...")
+myLabel1 = Label(root, text="Hello World!").grid(row=0, column=0)
+myLabel2 = Label(root, text="Goodbye World!").grid(row=1, column=2)
+myLabel3 = Label(root, text="I'll be back...").grid(row=2, column=3)
 
-# Grid - Shoving the label into the screen (.grid(where we want things))
-myLabel1.grid(row=0, column=0)
-myLabel2.grid(row=1, column=2) # Note: Code is relative to each other
-myLabel3.grid(row=2, column=3)
+# Grid - Shoving the label into the screen (.grid(where we want things)) 
+# Note: Code is relative to each other
+# Long way example: 
+#     myLabel1.grid(row=0, column=0)
+#     myLabel2.grid(row=1, column=2) 
+#     myLabel3.grid(row=2, column=3)
 
 # Creating a loop 
 root.mainloop()
